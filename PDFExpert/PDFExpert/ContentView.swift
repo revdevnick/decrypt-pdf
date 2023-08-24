11e9c3004c8235076a76275400fe3b70d755ba82
Nick Perkins <nickperkins@protonmail.com>
2023-08-24 14:14:59 -0400

    setup custom PDFView and updated ContentView. Need to resolve document error

--- PDFExpert/PDFExpert/ContentView.swift
+++ PDFExpert/PDFExpert/ContentView.swift
@@ -9,21 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var showActionSheet = false
+    @State private var showActionSheet: Bool = false
+    @State private var selectedPDFURL: URL = URL(fileURLWithPath: "")
+    @State private var isPDFViewActive: Bool = false
+    @State private var isAlertViewActive: Bool = false
+    @State private var errorMessage: String = ""
     
     var body: some View {
-        VStack {
-            Button {
-                self.showActionSheet.toggle()
-            } label: {
-                Text("Open PDF")
+        NavigationView {
+            VStack {
+                Button {
+                    self.showActionSheet.toggle()
+                } label: {
+                    Text("Open PDF")
+                }
+                .buttonStyle(RoundedRectangleButtonStyle())
+                .fileImporter(isPresented: $showActionSheet, allowedContentTypes: [.pdf]) { result in
+                    switch result {
+                    case .success(let pdfUrl):
+                        isPDFViewActive = true
+                        selectedPDFURL = pdfUrl
+                        let gotAccess = selectedPDFURL.startAccessingSecurityScopedResource()
+                        if !gotAccess { return }
+                        showActionSheet = false
+                    case .failure(let error):
+                        errorMessage = error.localizedDescription
+                        isAlertViewActive = true
+                        showActionSheet = false
+                    }
+                }
             }
-            .buttonStyle(RoundedRectangleButtonStyle())
-            .fileImporter(isPresented: $showActionSheet, allowedContentTypes: [.pdf]) { result in
-                print("!!! \(result)")
-            }
+            .padding()
         }
-        .padding()
+        .navigationDestination(isPresented: $isPDFViewActive, destination: {
+            MyPDFView(displayedPDFURL: selectedPDFURL)
+        })
+        .alert("Error Opening PDF", isPresented: $isAlertViewActive) {
+            Button("OK", role: .cancel) {
+                isAlertViewActive = false
+            }
+        } message: {
+            Text(self.errorMessage)
+        }
     }
 }
 
--- PDFExpert/PDFExpert/PDFViewer.swift
+++ PDFExpert/PDFExpert/PDFViewer.swift
@@ -8,36 +8,35 @@
 import SwiftUI
 import PDFKit
 
-struct CustomPDFView: View {
-
-  let displayedPDFURL: URL
-
-  var body: some View {
-    PDFKitRepresentedView(documentURL: displayedPDFURL)
-  }
+struct MyPDFView: View {
+    
+    let displayedPDFURL: URL
+    
+    var body: some View {
+        PDFKitView(documentURL: displayedPDFURL)
+    }
 }
 
-struct PDFKitRepresentedView: UIViewRepresentable {
-
-  let documentURL: URL
-
-  init(documentURL: URL) {
-    self.documentURL = documentURL
-  }
-
-  func makeUIView(context: Context) -> some UIView {
-
-    let pdfView = PDFView()
-
-    pdfView.document = PDFDocument(url: self.documentURL)
-    pdfView.autoScales = true
-    pdfView.displayMode = .singlePage
-    pdfView.usePageViewController(true)
-    pdfView.displayDirection = .horizontal
-
-    return pdfView
-  }
-
-  func updateUIView(_ uiView: UIViewType, context: Context) -> Void {
-  }
+struct PDFKitView: UIViewRepresentable {
+    
+    let documentURL: URL
+    
+    init(documentURL: URL) {
+        self.documentURL = documentURL
+    }
+    
+    func makeUIView(context: Context) -> some UIView {
+        
+        let pdfView = PDFView()
+        pdfView.document = PDFDocument(url: self.documentURL)
+        pdfView.autoScales = true
+        pdfView.displayMode = .singlePage
+        pdfView.usePageViewController(true)
+        pdfView.displayDirection = .horizontal
+        
+        return pdfView
+    }
+    
+    // we are not updating a PDF, so do nothing with this method
+    func updateUIView(_ uiView: UIViewType, context: Context) -> Void {}
 }
